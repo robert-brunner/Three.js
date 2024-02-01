41a5c5ea62ac651711de17fb4a96dc04a5ce2c29
Robert Brunner <brunner.robert.work@gmail.com>
2024-02-01 15:19:52 -0500

    Lesson 5 animations

--- lessons/05-animations/src/script.js
+++ lessons/05-animations/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import './style.css'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -23,9 +24,65 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+// //Time 
+// let time = Date.now()
+
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+
+//Clock
+const clock = new THREE.Clock()
+
+
+//Animations
+const tick = () =>{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    
+    //update objects
+    // mesh.rotation.x = elapsedTime * Math.PI * 2
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime) 
+    camera.position.y = Math.sin(elapsedTime)
+    camera.position.x = Math.cos(elapsedTime) 
+    camera.lookAt(mesh.position)
+    
+    //Render
+    renderer.render(scene, camera)
+    
+    window.requestAnimationFrame((tick))
+}
+
+tick()
+
+
+
+
+
+// //Animations
+// const tick = () =>{
+
+//     //Time
+//     const CurrentTime = Date.now()
+//     const deltaTime = CurrentTime - time
+//     time = CurrentTime
+
+
+
+//     //update objects
+//     mesh.rotation.x -= 0.001 * deltaTime
+//     // mesh.position.y -= 0.01
+
+//     //Render
+//     renderer.render(scene, camera)
+
+//     window.requestAnimationFrame((tick))
+// }
+
+// tick()
